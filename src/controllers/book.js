210d16e9116a0102859070a7c356c389d455196c
Joshua Nicholas Pearson <jnpearson4850@gmail.com>
2018-08-21 16:27:59 -0400

    fixing git issues

---
+++ src/controllers/book.js
@@ -0,0 +1,12 @@
+//psuedo code for controller
+// const Book = require("../models/book")
+// const Comment = require("../models/comment")
+
+// module.exports ={
+//     show: (req, res) => {
+//         Book.findOne({_id: req.params.id })
+//         .populate("title","author","description","image")
+
+
+//     }
+// }
