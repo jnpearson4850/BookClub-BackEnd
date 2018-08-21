ef7c61daf469bb6b945fac7c282ff14949f3a002
Joshua Nicholas Pearson <jnpearson4850@gmail.com>
2018-08-21 15:14:27 -0400

    corrected route for book

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.set("port", process.env.PORT || 3001);
 app.use(parser.json());
 app.use(methodOverride("_method"));
 
-app.use(require("./routes/routes.js"));
+
+app.use(require("./src/routes/index.js"));
 
 app.listen(app.get("port"), () => {
   console.log("Server listening on port " + app.get("port"));
--- src/routes/books.js
+++ src/routes/books.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const bookController = require("../controllers/book")
 
-router.get("/:id", bookController.show)
 router.get("/book/:id", bookController.show)
+router.get("/book", bookController.show)
 
-module.exports.router;
+module.exports = router;
